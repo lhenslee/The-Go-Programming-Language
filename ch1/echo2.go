d4ffc3ad4b7df0b27a25437dce7abb0565b14a1e
Lane Henslee <lane.henslee@arcturus-uav.com>
2021-09-02 14:49:40 -0700

    trying dup2 challenge

---
+++ ch1/echo2.go
@@ -0,0 +1,16 @@
+// Print command-line args
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
